761cb81d57924034591621698ea749666e37f8be
MJackson22-bit <jm5444859@gmail.com>
2023-01-21 21:09:39 -0600

    Handle errors and responses on Category

--- src/category/controllers/category.controller.ts
+++ src/category/controllers/category.controller.ts
@@ -1,15 +1,23 @@
 import { Request, Response } from "express";
+import { DeleteResult, UpdateResult } from "typeorm";
+import { HttpResponse } from "../../shared/response/http.response";
 import { CategoryService } from "../services/category.service";
 
 export class CategoryController {
-    constructor(private readonly categoryService: CategoryService = new CategoryService()) {}
+    constructor(
+        private readonly categoryService: CategoryService = new CategoryService(),
+        private readonly httpResponse: HttpResponse = new HttpResponse()
+    ) {}
 
     async getCategories(req: Request, res: Response) {
         try {
             const data = await this.categoryService.findAllCategories();
-            res.status(200).json(data);
+            if(data.length == 0) {
+                return this.httpResponse.NotFound(res, 'No categories found')
+            }
+            return this.httpResponse.Ok(res, data)
         } catch (error) {
-            console.error(error);
+            return this.httpResponse.Error(res, error)
         }
     }
 
@@ -17,38 +25,47 @@ export class CategoryController {
         const { id } = req.params
         try {
             const data = await this.categoryService.findByCategoryId(id)
-            res.status(200).json(data);
+            if(!data) {
+                return this.httpResponse.NotFound(res, 'Category not found')
+            }
+            return this.httpResponse.Ok(res, data)
         } catch (error) {
-            console.error(error);
+            return this.httpResponse.Error(res, error)
         }
     }
 
     async createCategory(req: Request, res: Response) {
         try {
             const data = await this.categoryService.createCategory(req.body)
-            res.status(200).json(data);
+            return this.httpResponse.Ok(res, data)
         } catch (error) {
-            console.error(error);
+            return this.httpResponse.Error(res, error)
         }
     }
 
     async updateCategory(req: Request, res: Response) {
         const { id } = req.params
         try {
-            const data = await this.categoryService.updateCategory(id, req.body)
-            res.status(200).json(data)
+            const data: UpdateResult = await this.categoryService.updateCategory(id, req.body)
+            if(!data.affected){
+                return this.httpResponse.NotFound(res, "Error updating category")
+            }
+            return this.httpResponse.Ok(res, data)
         } catch (error) {
-            console.error(error)
+            return this.httpResponse.Error(res, error)
         }
     }
 
     async deleteCategory(req: Request, res: Response) {
         const { id } = req.params
         try {
-            const data = await this.categoryService.deleteCategory(id)
-            res.status(200).json(data)
+            const data: DeleteResult = await this.categoryService.deleteCategory(id)
+            if(!data.affected){
+                return this.httpResponse.NotFound(res, "Error deleting category")
+            }
+            return this.httpResponse.Ok(res, data)
         } catch (error) {
-            console.error(error)
+            return this.httpResponse.Error(res, error)
         }
     }
 }
